7917cb628e21a912b3dbda9439726a7a5b901f88
U-CHOC\JeongHoon <olzlzl@gmail.com>
2016-11-21 21:37:21 +0900

    initial commit

---
+++ src/main/java/choc/Choc01Application.java
@@ -0,0 +1,12 @@
+package choc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Choc01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Choc01Application.class, args);
+	}
+}
---
+++ src/test/java/choc/Choc01ApplicationTests.java
@@ -0,0 +1,16 @@
+package choc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Choc01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
